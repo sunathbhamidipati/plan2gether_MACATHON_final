7c44fd433935daf4621ed8cdeae6e9356dad716f
Besat Master <sunathhulk@gmail.com>
2025-04-26 05:30:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./src/services/events.json
    > Code generation for chunk item errored
    > An error occurred while generating the chunk item [project]/src/services/events.json (json)
    >
    > Caused by:
    > - Unable to make a module from invalid JSON: EOF while parsing a list at line 63 column 4
    >
    > Debug info:
    > - An error occurred while generating the chunk item [project]/src/services/events.json (json)
    > - Execution of <JsonChunkItem as EcmascriptChunkItem>::content failed
    > - Unable to make a module from invalid JSON: EOF while parsing a list at line 63 column 4
    >     at .
    >       59 |     "spotsAvailable": 6,
    >       60 |     "date": "2025-01-30"
    >       61 |   }
    >       62 |
    >          |    v
    >       63 +
    >          |    ^
    >
    >

--- src/services/event.ts
+++ src/services/event.ts
@@ -1,3 +1,5 @@
+import eventsData from './events.json';
+
 /**
  * Represents a tag.
  */
@@ -38,67 +40,5 @@ export interface Event {
  */
 export async function getEvents(query: string): Promise<Event[]> {
   // TODO: Implement this by calling an API.
-
-  return [
-    {
-      description: 'Crochet by the river',
-      tags: [{ name: 'crochet' }, { name: 'river' }],
-      spotsAvailable: 50,
-      date: new Date(2025, 4, 2),
-    },
-    {
-      description: 'Friends and fun: crochet day',
-      tags: [{ name: 'friends' }, { name: 'crochet' }],
-      spotsAvailable: 25,
-      date: new Date(2025, 4, 16),
-    },
-    {
-      description: 'Make your own yarn plush',
-      tags: [{ name: 'crochet' }, { name: 'create' }, { name: 'socialise' }],
-      spotsAvailable: 15,
-      date: new Date(2025, 5, 30),
-    },
-    {
-      description: 'Sip and paint',
-      tags: [{ name: 'paint' }, { name: 'art' }, { name: 'beverages' }],
-      spotsAvailable: 40,
-      date: new Date(2025, 4, 21),
-    },
-    {
-      description: 'Lazy beach afternoon',
-      tags: [{ name: 'beach' }, { name: 'relax' }],
-      spotsAvailable: 23,
-      date: new Date(2025, 5, 2),
-    },
-    {
-      description: 'A fun run in the park.',
-      tags: [{ name: 'running' }, { name: 'outdoors' }],
-      spotsAvailable: 10,
-      date: new Date(2024, 8, 10),
-    },
-    {
-      description: 'A workshop on pottery.',
-      tags: [{ name: 'arts' }, { name: 'crafts' }],
-      spotsAvailable: 5,
-       date: new Date(2024, 9, 15),
-    },
-    {
-      description: 'A relaxing yoga session.',
-      tags: [{ name: 'yoga' }, { name: 'wellness' }],
-      spotsAvailable: 12,
-      date: new Date(2024, 10, 20),
-    },
-    {
-      description: 'A thrilling rock climbing adventure.',
-      tags: [{ name: 'climbing' }, { name: 'adventure' }],
-      spotsAvailable: 8,
-      date: new Date(2024, 11, 25),
-    },
-    {
-      description: 'A creative writing workshop.',
-      tags: [{ name: 'writing' }, { name: 'arts' }],
-      spotsAvailable: 6,
-      date: new Date(2025, 0, 30),
-    },
-  ];
+  return eventsData as Event[];
 }
